47522c0ab56fdcd5c3e2b0912640f57396a27a73
malabarMCB <grinevich97@gmail.com>
2017-04-24 13:21:37 +0300

    Add edit page and keyup enter in search news input

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,14 @@ import {RouterModule} from '@angular/router'
 
 //modules
 import{HomeModule} from './home.module/home.module'
+import {EditModule} from "./edit.module/edit.module";
 
 @NgModule({
     imports:      [ RouterModule.forRoot([
         {path:'', redirectTo:'/home', pathMatch:'full'}
     ]),
-        BrowserModule, FormsModule,HomeModule ],
-    declarations: [ AppComponent ],
+        BrowserModule, FormsModule,HomeModule,EditModule ],
+    declarations: [ AppComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule {
--- app/home.module/search-text.component/search-text.component.ts
+++ app/home.module/search-text.component/search-text.component.ts
@@ -3,14 +3,14 @@ import {Component, EventEmitter, Output} from '@angular/core'
 @Component({
     moduleId:module.id,
     selector:'text-search',
-    templateUrl:'./text-search.component.html',
-    styleUrls:['./text-search.component.css','../home.module.css']
+    templateUrl:'./search-text.component.html',
+    styleUrls:['./search-text.component.css','../home.module.css']
 })
 export class TextSearchComponent{
     @Output()
     searchRequested=new EventEmitter<string>();
 
-    clickHandler(topicsSearchText:string){
+    sendRequest(topicsSearchText:string){
         this.searchRequested.emit(topicsSearchText);
     }
 }
